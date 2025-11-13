1fc8c01e5cf976e56064081405b671f9451266a1
adiprasetia <adiprasetia1@gmail.com>
2025-11-13 14:33:26 +0700

    REvisi Sedikit untuk order tabel ada yang salah simpan

--- app/Filament/Resources/Orders/Tables/OrdersTable.php
+++ app/Filament/Resources/Orders/Tables/OrdersTable.php
@@ -8,7 +8,6 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\Layout\Panel;
 use Filament\Tables\Table;
 
 class OrdersTable
